b48cd48d73eee4d8cdd5b92386e552fa751468fc
Enoka <ejaona@gmail.com>
2021-05-26 05:17:47 -0500

    failing test success

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const server = express()
 
-server.get('/home', (req, res) => res.send('Hello docker'))
+server.get('/', (req, res) => res.send('Hello docker'))
 
 server.listen(3000, () => console.log('running on port 3k'))
